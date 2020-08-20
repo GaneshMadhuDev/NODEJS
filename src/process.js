823424c81222708cff9ae3be3466288376d74dbd
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-19 19:49:21 -0500

    Process js

---
+++ src/process.js
@@ -0,0 +1,17 @@
+
+    console.log(process.argv);
+
+    // In Command line
+    // ls
+    // cd src
+    // run node process.js => gives
+    //[ '/usr/local/bin/node',
+    //       '/Users/ganeshmadhu/Desktop/NodeJs/src/process.js' ]
+
+    console.log(process.pid);
+
+    // 83732 --> gives pid
+
+    console.log(process.versions.node);
+
+    // gives => 10.16.3 --> version of node
