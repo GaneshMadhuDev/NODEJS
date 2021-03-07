ff86491623ae0ca1c347340c3019f084a958a880
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-03-07 09:56:23 -0600

    Closoures example

--- src/closures.js
+++ src/closures.js
@@ -17,3 +17,19 @@
          };
 
     }
+
+
+
+    var count = (function () {
+
+         var _counter = 0;
+
+         return function () {return _counter += 1;}
+
+    })();
+
+    count();
+    count();
+    count();
+
+    // the counter is now 3
